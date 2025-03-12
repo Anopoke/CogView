1ad30e86c0a84e6a87a0bfcdc5d2cb76e3bf40a1
CrystalChen <113891083+Anopoke@users.noreply.github.com>
2025-03-12 11:18:17 +0800

    Update CogView4.py

--- CogView4.py
+++ CogView4.py
@@ -56,7 +56,7 @@ class CogView4(CogView4Pipeline):
             f"{self.model_path}/transformer",
             # quantization_config=BitsAndBytesConfig(
             #     bnb_4bit_quant_type="nf4",
-            #     bnb_4bit_compute_dtype=torch_dtype,
+            #     bnb_4bit_compute_dtype=self.torch_dtype,
             #     bnb_4bit_use_double_quant=True
             # ),
             torch_dtype=self.torch_dtype,
